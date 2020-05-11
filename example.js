fc248d333d65656525e38a4abecb85d3ef7f2469
Taylor Callsen <gritto2008@gmail.com>
2020-05-11 11:36:18 -0700

    fixing line measurement

--- example.js
+++ example.js
@@ -3,9 +3,8 @@
 // load dependencies 
 const fs = require('fs')
 
-var parse = require('wellknown');
+var parse = require('wellknown')
 const turf = require('@turf/turf')
-require('@turf/length')
 
 // open sample file
 const sampleWKT = fs.readFileSync('./sample_data/Sample_Stintson_Beach.wkt', 'utf8')
@@ -14,6 +13,6 @@ const sampleWKT = fs.readFileSync('./sample_data/Sample_Stintson_Beach.wkt', 'ut
 
 const linestring1 = turf.lineString(parse(sampleWKT).coordinates)
 
-console.log(turf.lineDistance(linestring1))
+console.log(turf.length(linestring1, {units: 'miles'}))
 
 // reporting 15miles, strava says 9.. do I need to simplify the geometry first?
