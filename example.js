a3beb63e9d30b280402a1b7c3a15661e16afa5d6
Taylor Callsen <gritto2008@gmail.com>
2020-05-11 12:11:02 -0700

    adding linear referencing calculations

--- example.js
+++ example.js
@@ -9,10 +9,37 @@ const turf = require('@turf/turf')
 // open sample file
 const sampleWKT = fs.readFileSync('./sample_data/Sample_Stintson_Beach.wkt', 'utf8')
 
-// convery to GeoJSON
+// convert to GeoJSON
+const linestringJSON = parse(sampleWKT).coordinates
 
-const linestring1 = turf.lineString(parse(sampleWKT).coordinates)
+// load into turf linestring obj
+const linestring = turf.lineString(linestringJSON)
 
-console.log(turf.length(linestring1, {units: 'miles'}))
+// add linear referencing - 4th "M" dimension
+let previousLength = 0 // track previous length
+let previousCoord = null
+linestring.geometry.coordinates.forEach( (currentCoord, index) => {
+  
+  const currentLength = 0
 
-// reporting 15miles, strava says 9.. do I need to simplify the geometry first?
+  // if first point set length to 0, otherwise append length to end of point array
+  if (index===0) {
+    currentCoord.push(0)
+  } else {
+    const distance = turf.distance(previousCoord, currentCoord, {units: 'miles'})
+    currentCoord.push(previousCoord[3] + distance)
+  }
+
+  previousLength = currentLength
+  previousCoord = currentCoord
+
+})
+
+// confirm total line length and cumulative lengths match
+// console.log('total length:', turf.length(linestring, {units: 'miles'}))
+// console.log('cum length:', linestring.geometry.coordinates[linestring.geometry.coordinates.length-1][3])
+
+// output back to WKT (including 4th dimension)
+const outputWKT = parse.stringify(linestring)
+
+console.log(outputWKT)
